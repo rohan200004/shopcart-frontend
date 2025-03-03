edbebd8261719b5ae952a4a43a61a2169f2a4979
shivakokkula <kokkulashivakumar1@gmail.com>
2025-03-04 00:24:53 +0530

    commit

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -44,33 +44,33 @@ const PaymentPage = ({ clearCart }) => {
       } else {
         const response = await api.post("/order", order);
         if (response.status === 201) {
-          const order_id = response.data.id;
-          const options = {
-            key: 'rzp_test_zHqDbIZs1T1BdQ',  // Replace with your Razorpay public key
-            amount: total,          // Amount in paise
-            currency: 'INR',
-            name: 'Your Company Name',
-            description: 'Test Payment',
-            order_id,  // Order ID from the backend
-            handler: function (response) {
-              alert('Payment Successful');
-              console.log(response);
-            },
-            prefill: {
-              name: 'John Doe',
-              email: 'john.doe@example.com',
-              contact: '9876543210',
-            },
-            notes: {
-              address: 'Razorpay Corporate Office',
-            },
-            theme: {
-              color: '#F37254',
-            },
-          };
+          // const order_id = response.data.id;
+          // const options = {
+          //   key: 'rzp_test_zHqDbIZs1T1BdQ',  // Replace with your Razorpay public key
+          //   amount: total,          // Amount in paise
+          //   currency: 'INR',
+          //   name: 'Your Company Name',
+          //   description: 'Test Payment',
+          //   order_id,  // Order ID from the backend
+          //   handler: function (response) {
+          //     alert('Payment Successful');
+          //     console.log(response);
+          //   },
+          //   prefill: {
+          //     name: 'John Doe',
+          //     email: 'john.doe@example.com',
+          //     contact: '9876543210',
+          //   },
+          //   notes: {
+          //     address: 'Razorpay Corporate Office',
+          //   },
+          //   theme: {
+          //     color: '#F37254',
+          //   },
+          // };
 
-          const rzp1 = new window.Razorpay(options);
-          rzp1.open();
+          // const rzp1 = new window.Razorpay(options);
+          // rzp1.open();
           if (response.data.message === "Created") {
             await new Promise(resolve => setTimeout(resolve, 1500));
             await clearCart();
